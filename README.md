a816724f458a32f35b8981f25c6fd037b416ab48
NataliiaMolch <mnatasha9@gmail.com>
2023-11-14 14:56:43 +0100

    isbi_paper

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# MS_WML_uncs
+# Multi-scale comparative study of uncertainty measures for white matter lesion segmentation
+
 This repository contains the code used for the study of voxel- and lesion- scale uncertainty in application to 
-white matter multiple sclerosis segmentation task. 
+white matter multiple sclerosis segmentation task. It is supplementary to our [paper](https://arxiv.org/abs/2211.04825).
 
 The code includes:
 * Implementations of voxel-scale uncertainty measures, such as mutual information (MI), expected pair-wise KL divergence (EPKL) and reverse mutual information (RMI) for knowledge uncertainty; expected entropy (ExE) for data uncertainty; entropy of expected (EoE) and negated confidence (NC).
 * Implementations of lesion-scale uncertainty measures, such as mean or logsum of voxels' uncertainties within the lesion region, proposed _Detection disagreement uncertainty_ (DDU).
 * Implementation of the retention curves on the voxel and lesion scales.
+
+Requirements
+---
+
+Code wads compiled with python 3.8. All the requirements are listed in "requirements.txt" and can be installed using a pip command:
+
+```commandline
+pip3 install -r requirements.txt
+```
 
 Model
 ----
@@ -22,3 +32,46 @@ The "train" and "dev in" sets were used for training and validation. The "eval i
 Code organisation
 ----
 
+The code contains several modules with aforementioned implementations. All the functions inside the modules contain docstring documentation.
+
+### Module "voxel_uncertainty_measures.py"
+
+Contains implementations of voxel-scale uncertainty measures. Function
+`ensemble_uncertainties_classification(probs, epsilon) -> dict` allows obtaining 
+all the uncertainty maps for a single scan described in the paper.
+
+### Module "lesion_uncertainty_measures.py"
+
+Contains implementations of lesion-scale uncertainty measures described in the paper. Function 
+`lesions_uncertainty(y_pred_multi, vox_unc_maps, ens_pred_multi, ens_pred_multi_true, n_jobs, dl)` allows obtaining
+lesion uncertainty measures for each lesion in the scan. Function `lesions_uncertainty_maps(y_pred_multi, vox_unc_maps, ens_pred_multi, ens_pred_multi_true, n_jobs, dl) -> dict` allows obtaining
+different lesion uncertainty maps for a particular scans.
+
+### Module "error_retention_curves.py"
+
+Contains implementations of voxel- and lesion- scale retention curves.
+Function `voxel_scale_rc(y_pred, y, uncertainties, fracs_retained, metric_name, n_jobs) -> tuple` given uncertainty estimates builds voxel scale retention curve for a chosen segmentation quality metric and computes area above the retention curve for a single scan.
+Function `lesion_scale_rc_nofn(lesion_uncertainties, lesion_types, n_fn, fracs_retained) -> tuple` given uncertainty estimates builds lesion F1 retention curve for a particular scan, it does not remove false negative lesions (used in the paper).
+Function `lesion_scale_rc(--"--)` does the same, but excludes false negative lesions, thus provides a more theoretical evaluation of an uncertainty measure.
+
+### Module "lesion_extraction.py"
+
+Contains a function to extract TP, FP and FN lesions `get_tp_fp_fn_lesions(...)` given predicted segmentation map and a ground truth, 
+as well as a function to count the number of FN lesions `gt_fn_count(...)`.
+
+Citation
+---
+
+In case of using this code, please cite:
+
+```
+@misc{https://doi.org/10.48550/arxiv.2211.04825,
+  doi = {10.48550/ARXIV.2211.04825},
+  url = {https://arxiv.org/abs/2211.04825},
+  author = {Molchanova, Nataliia and Raina, Vatsal and Malinin, Andrey and La Rosa, Francesco and Muller, Henning and Gales, Mark and Granziera, Cristina and Graziani, Mara and Cuadra, Meritxell Bach},
+  title = {Novel structural-scale uncertainty measures and error retention curves: application to multiple sclerosis},
+  publisher = {arXiv},
+  year = {2022},
+  copyright = {Creative Commons Attribution 4.0 International}
+}
+```
--- error_retention_curves.py
+++ error_retention_curves.py
@@ -1,3 +1,6 @@
+"""
+Implementation of voxel and lesion scale retention curves.
+"""
 import numpy as np
 from functools import partial
 from collections import Counter
@@ -6,13 +9,13 @@ from sklearn import metrics
 from scipy.interpolate import interp1d
 
 
-def voxel_scale_metrics(y_pred: np.ndarray, y: np.ndarray, r: float = 0.001):
+def voxel_scale_metrics(y_pred: np.ndarray, y: np.ndarray, r: float = 0.001) -> dict:
     """
     Compute DSC, nDSC, TPR, TDR, FNR.
-    :param y_pred:
-    :param y:
-    :param r: effective lesion load
-    :return: dict
+    :param y_pred: predicted binary segmentation mask, shape [H, W, D]
+    :param y: ground truth binary segmentation mask, shape [H, W, D]
+    :param r: effective lesion load for nDSC computation
+    :return: dict of voxel-scale metrics
     """
     if np.sum(y) + np.sum(y_pred) == 0:
         return {'DSC': 1.0, 'nDSC': 1.0, 'TPR': 1.0, 'TDR': 1.0, 'FNR': 0.0}
@@ -29,16 +32,18 @@ def voxel_scale_metrics(y_pred: np.ndarray, y: np.ndarray, r: float = 0.001):
         }
 
 
-def voxel_scale_rc(y_pred: np.ndarray, y: np.ndarray, uncertainties: np.ndarray, fracs_retained: np.ndarray, metric_name: str,
-                   n_jobs: int = None):
+def voxel_scale_rc(y_pred: np.ndarray, y: np.ndarray, uncertainties: np.ndarray,
+                   fracs_retained: np.ndarray = np.linspace(0.0, 1.0, 400),
+                   metric_name: str = 'DSC', n_jobs: int = None) -> tuple:
     """
-    Compute error retention curve values and nDSC-AAC on voxel-scale.
-    :param fracs_retained:
-    :param y:
-    :param y_pred:
-    :param uncertainties:
+    Compute `metric_name` retention curve values and nDSC-AAC on voxel-scale.
+    :param y: flattened ground truth binary lesion mask, shape [H*W*D,]
+    :param y_pred: flattened predicted binary lesion mask, shape [H*W*D,]
+    :param uncertainties: flattened voxel-scale uncertainty map, shape [H*W*D]
+    :param fracs_retained: array with fractions of retained voxels, shape [N,]
+    :param metric_name: name of metric to be used for building a retention curve, from the ones returned by `voxel_scale_metrics()`
     :param n_jobs: number of parallel processes
-    :return: tuple with nDSC-AAC and nDSC values of the error retention curve
+    :return: tuple with `metric_name`-AAC (float) and `metric_name`-RC values corresponding to each value of `fracs_retained`, shape [N,]
     """
 
     def compute_metric(frac_, preds_, gts_, N_, metric_name_):
@@ -57,54 +62,40 @@ def voxel_scale_rc(y_pred: np.ndarray, y: np.ndarray, uncertainties: np.ndarray,
     return 1. - metrics.auc(fracs_retained, np.asarray(dsc_norm_scores)), dsc_norm_scores
 
 
-def lesion_scale_rc(tp_les_uncs: list, fp_les_uncs: list, fn_les_uncs: list, fracs_retained: np.ndarray):
+def lesion_scale_rc(lesion_uncertainties: np.ndarray, lesion_types: np.ndarray,
+                    fracs_retained: np.ndarray = np.linspace(0.0, 1.0, 400)) -> tuple:
     """
-    Compute error retention curve values and F1les-AAC on lesion-scale including fasle negative lesions.
-    :param tp_les_uncs:
-    :param fp_les_uncs:
-    :param fn_les_uncs:
-    :type fracs_retained: numpy.ndarray [n_fr,]
+    Compute lesion F1-retention curve values and lesion F1-AAC on lesion-scale including false negative lesions.
+    :param lesion_uncertainties: uncertainty values for each TP, FP and FN lesion of a particular scan, shape [N_TP + N_FP + N_FN,]
+    :param lesion_types: array with lesion types, i.e. 'tp', 'fp', 'fn', corresponding to `lesion_uncertainties` array, shape [N_TP + N_FP + N_FN,]
+    :param fracs_retained: array with fractions of retained voxels, shape [N,]
     :return: F1les-AAC, values of error retention curve, values of error retention curve before interpolation.
-    :rtype: tuple
+    :return: tuple with lesion F1-AAC (float) and lesion F1-RC values corresponding to each value of `fracs_retained`, shape [N,]
     """
 
-    def compute_f1(lesion_types):
-        counter = Counter(lesion_types)
+    def compute_f1(lt):
+        counter = Counter(lt)
         if counter['tp'] + 0.5 * (counter['fp'] + counter['fn']) == 0.0:
             return 0
         return counter['tp'] / (counter['tp'] + 0.5 * (counter['fp'] + counter['fn']))
 
-    # list of lesions uncertainties tp, fp, fn
-    uncs_list = [np.asarray(tp_les_uncs),
-                 np.asarray(fp_les_uncs),
-                 np.asarray(fn_les_uncs)]
-    # list of lesion types
-    lesion_type_list = [np.full(shape=unc.shape, fill_value=fill)
-                        for unc, fill in zip(uncs_list, ['tp', 'fp', 'fn'])
-                        if unc.size > 0]
-    uncs_list = [unc for unc in uncs_list if unc.size > 0]
-    uncs_all = np.concatenate(uncs_list, axis=0)
-    lesion_type_all = np.concatenate(lesion_type_list, axis=0)
-
-    del uncs_list, lesion_type_list
-    assert uncs_all.shape == lesion_type_all.shape
+    assert lesion_uncertainties.shape == lesion_types.shape
 
     # sort uncertainties and lesions types
-    ordering = uncs_all.argsort()
-    lesion_type_all = lesion_type_all[ordering][::-1]
+    ordering = lesion_uncertainties.argsort()
+    lesion_types = lesion_types[ordering][::-1]
 
-    f1_values = [compute_f1(lesion_type_all)]
-
-    # reject the most certain lesion
-    for i_l, lesion_type in enumerate(lesion_type_all):
+    f1_values = [compute_f1(lesion_types)]
+    # reject the most uncertain lesion
+    for i_l, lesion_type in enumerate(lesion_types):
         if lesion_type == 'fp':
-            lesion_type_all[i_l] = 'tn'
+            lesion_types[i_l] = 'tn'
         elif lesion_type == 'fn':
-            lesion_type_all[i_l] = 'tp'
-        f1_values.append(compute_f1(lesion_type_all))
+            lesion_types[i_l] = 'tp'
+        f1_values.append(compute_f1(lesion_types))
 
     # interpolate the curve and make predictions in the retention fraction nodes
-    n_lesions = lesion_type_all.shape[0]
+    n_lesions = lesion_types.shape[0]
     spline_interpolator = interp1d(x=[_ / n_lesions for _ in range(n_lesions + 1)],
                                    y=f1_values[::-1],
                                    kind='slinear', fill_value="extrapolate")
@@ -112,49 +103,39 @@ def lesion_scale_rc(tp_les_uncs: list, fp_les_uncs: list, fn_les_uncs: list, fra
     return 1. - metrics.auc(fracs_retained, np.asarray(f1_interp)), f1_interp
 
 
-def lesion_scale_rc_nofn(tp_les_uncs: list, fp_les_uncs: list, n_fn: int, fracs_retained: np.ndarray):
+def lesion_scale_rc_nofn(lesion_uncertainties: np.ndarray, lesion_types: np.ndarray, n_fn: int,
+                         fracs_retained: np.ndarray = np.linspace(0.0, 1.0, 400)) -> tuple:
     """
-    Compute error retention curve values and F1les-AAC on lesion-scale without false negative lesions.
-    :type fracs_retained: numpy.ndarray [n_fr,]
-    :return: F1les-AAC, values of error retention curve, values of error retention curve before interpolation.
-    :rtype: tuple
+    Compute lesion F1-retention curve values and lesion F1-AAC on lesion-scale without false negative lesions.
+    :param lesion_uncertainties: uncertainty values for each TP, FP lesion of a particular scan, shape [N_TP + N_FP,]
+    :param lesion_types: array with lesion types, i.e. 'tp', 'fp', corresponding to `lesion_uncertainties` array, shape [N_TP + N_FP,]
+    :param n_fn: number of false negative lesions in this scan
+    :param fracs_retained: array with fractions of retained voxels, shape [N,]
+    :return: tuple with lesion F1-AAC (float) and lesion F1-RC values corresponding to each value of `fracs_retained`, shape [N,]
     """
 
-    def compute_f1(lesion_types, num_fn):
-        counter = Counter(lesion_types)
+    def compute_f1(lt, num_fn):
+        counter = Counter(lt)
         if counter['tp'] + 0.5 * (counter['fp'] + num_fn) == 0.0:
             return 0.
         return counter['tp'] / (counter['tp'] + 0.5 * (counter['fp'] + num_fn))
 
-    # list of lesions uncertainties tp, fp, fn
-    uncs_list = [np.asarray(tp_les_uncs), np.asarray(fp_les_uncs)]
-    # list of lesion types
-    lesion_type_list = [np.full(shape=unc.shape, fill_value=fill)
-                        for unc, fill in zip(uncs_list, ['tp', 'fp'])
-                        if unc.size > 0]
-    uncs_list = [unc for unc in uncs_list if unc.size > 0]
-    uncs_all = np.concatenate(uncs_list, axis=0)
-    lesion_type_all = np.concatenate(lesion_type_list, axis=0)
-
-    del uncs_list, lesion_type_list
-    assert uncs_all.shape == lesion_type_all.shape
+    assert lesion_uncertainties.shape == lesion_types.shape
 
     # sort uncertainties and lesions types
-    ordering = uncs_all.argsort()
-    lesion_type_all = lesion_type_all[ordering][::-1]
+    ordering = lesion_uncertainties.argsort()
+    lesion_types = lesion_types[ordering][::-1]
 
-    f1_values = [compute_f1(lesion_type_all, num_fn=n_fn)]
+    f1_values = [compute_f1(lesion_types, num_fn=n_fn)]
 
-    # reject the most certain lesion
-    for i_l, lesion_type in enumerate(lesion_type_all):
+    # reject the most uncertain lesion
+    for i_l, lesion_type in enumerate(lesion_types):
         if lesion_type == 'fp':
-            lesion_type_all[i_l] = 'tn'
-        elif lesion_type == 'fn':
-            lesion_type_all[i_l] = 'tp'
-        f1_values.append(compute_f1(lesion_type_all, num_fn=n_fn))
+            lesion_types[i_l] = 'tn'
+        f1_values.append(compute_f1(lesion_types, num_fn=n_fn))
 
     # interpolate the curve and make predictions in the retention fraction nodes
-    n_lesions = lesion_type_all.shape[0]
+    n_lesions = lesion_types.shape[0]
     spline_interpolator = interp1d(x=[_ / n_lesions for _ in range(n_lesions + 1)],
                                    y=f1_values[::-1],
                                    kind='slinear', fill_value="extrapolate")
